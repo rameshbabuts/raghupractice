a03c7f7f6a4191b1d5370c403a4fbf7f6870bf7a
rameshbabuts <rams134.reddy@gmail.com>
2023-09-19 16:18:39 +0530

    New

---
+++ git.sh
@@ -0,0 +1,7 @@
+echo "# raghupractice" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:rameshbabuts/raghupractice.git
+git push -u origin main
